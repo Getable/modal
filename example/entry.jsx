500d5a8f41a3a5871f4a37fd1e623cbbda655b8f
Joey Baker <joey@byjoeybaker.com>
2015-05-15 12:10:19 -0700

    internal: better example
    * added a button to open another modal
    * added a button to close the modal
    * added a bunch of dummy content so that we can see what it looks like

--- example/entry.jsx
+++ example/entry.jsx
@@ -5,4 +5,21 @@ import data from './data.js'
 // expose React for debugging
 window.React = React
 
-Modal.open(<div><Modal {...data}><button>This button should not close the modal</button></Modal></div>)
+const appEl = document.getElementById('app')
+const modal = (
+  <Modal {...data}>
+    <button>This button should not close the modal</button>
+    <button onClick={Modal.close}>Close modal</button>
+  </Modal>
+)
+const openModal = () => Modal.open(modal)
+// override the server render
+React.render(
+  <div>
+    <button onClick={openModal}>Open modal</button>
+    {new Array(1e4).join('content ')}
+  </div>
+, appEl)
+
+// open the modal to start
+openModal()
